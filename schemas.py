9ce1129d5f3ed7d5eb42a1277b55f9a215452615
nikolay_bogdan <dhjan44@gmail.com>
2023-09-20 19:00:16 +0300

    d

---
+++ schemas.py
@@ -0,0 +1,22 @@
+from marshmallow import Schema, fields, validates_schema, ValidationError
+
+
+class RequestParamsSchema(Schema):
+    cmd = fields.Str(required=True)
+    value = fields.Str(required=True)
+
+
+
+    @validates_schema
+    def validate_cmd_param(self, values, *args, **kwargs):
+        valid_cmd_commands = {'filter', 'sort', 'map', 'limit', 'unique'}
+
+        if values['cmd'] not in valid_cmd_commands:
+            raise ValidationError({'cmd':f'contains invalid command={values["cmd"]}'})
+
+        return values
+
+
+class RequestParamsListSchema(Schema):
+    queries = fields.Nested(RequestParamsSchema, many=True)
+    file_name = fields.Str(required=True)
